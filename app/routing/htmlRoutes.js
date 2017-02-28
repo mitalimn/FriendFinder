62ef83929d18aa6f3f0455284f3b25fa94cb5ba5
Mitali Naik <mitalinaik@yahoo.com>
2017-02-28 10:16:45 -0600

    htmlroute

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    app.get("/", function(req, res) {
+    app.use("/", function(req, res) {
         // body...
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
